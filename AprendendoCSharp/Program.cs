e4773b46f8b6e2c088f1dfabe80b22ea81de613d
Leonardo Webster <leonardowebster15@gmail.com>
2022-07-15 15:14:51 -0300

    Primeiro commit

---
+++ AprendendoCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo. Projeto no Visual Studio!");
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
