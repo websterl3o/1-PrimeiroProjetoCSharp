56a4f8517e7bc024f14d5d0ec4139fc137d94b0c
Leonardo Webster <leonardowebster15@gmail.com>
2022-07-18 09:21:18 -0300

    Corrigindo estrutura dos arquivos

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _2_criandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade;
+            idade = 10;
+            Console.WriteLine("A idade é: " + idade);
+
+            idade = 32;
+            Console.WriteLine("A idade mudou, ela agora é: " + idade);
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
